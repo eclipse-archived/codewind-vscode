460eb7176acfaf2aca9bb9375022a8c00b8225bf
Tim Etchells <timetchells@ibm.com>
2019-09-18 17:56:50 -0400

    Fix wrong language detection for extension project
    https://github.com/eclipse/codewind/issues/486
    
    Signed-off-by: Tim Etchells <timetchells@ibm.com>

--- dev/src/codewind/project/ProjectType.ts
+++ dev/src/codewind/project/ProjectType.ts
@@ -120,6 +120,10 @@ export class ProjectType {
         }
     }
 
+    public static getRecognizedInternalTypes(): ProjectType.InternalTypes[] {
+        return Object.values(this.InternalTypes);
+    }
+
     /*
     private static getUserFriendlyType(type: ProjectType.Types): string {
         // For docker projects, return the language
