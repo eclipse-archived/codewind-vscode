eb1fa8adc120353f733e47c3f93afd7bdcabf149
Tim Etchells <timetchells@ibm.com>
2019-10-24 13:03:35 -0400

    Recreate local connection on refresh if URL change, plus add some error logging (#250)
    * Recreate local connection on refresh if URL change
    
    You can now restart Codewind outside of VS Code, and then refresh to pick it up
    
    Signed-off-by: Tim Etchells <timetchells@ibm.com>
    
    * Show errors thrown on project create
    
    Happens if invalid project data is sent by backend
    
    Signed-off-by: Tim Etchells <timetchells@ibm.com>

--- dev/src/command/connection/RefreshConnectionCmd.ts
+++ dev/src/command/connection/RefreshConnectionCmd.ts
@@ -14,8 +14,18 @@ import * as vscode from "vscode";
 import Connection from "../../codewind/connection/Connection";
 import Translator from "../../constants/strings/translator";
 import StringNamespaces from "../../constants/strings/StringNamespaces";
+import LocalCodewindManager from "../../codewind/connection/local/LocalCodewindManager";
 
 export default async function refreshConnectionCmd(connection: Connection): Promise<void> {
+
+    if (!connection.isRemote) {
+       const localHasChanged = await LocalCodewindManager.instance.refresh();
+       if (localHasChanged) {
+           vscode.window.showInformationMessage(`Reconnected to Local Codewind`);
+           return;
+       }
+    }
+
     vscode.window.showInformationMessage(Translator.t(StringNamespaces.CMD_MISC, "refreshedConnection"));
     return connection.forceUpdateProjectList(true);
 }
