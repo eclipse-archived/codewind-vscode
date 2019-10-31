71c57bf50b70e5257a72fd5620b3ceea21015af2
Laven S <42549570+laven26@users.noreply.github.com>
2019-10-31 17:46:04 -0400

    remote connection ui (#256)
    * remote connection
    
    Signed-off-by: laven-s <laven.s@ibm.com>
    
    * remove delete button before connection
    
    Signed-off-by: laven-s <laven.s@ibm.com>
    
    * remove unused svg constant
    
    Signed-off-by: laven-s <laven.s@ibm.com>

--- dev/src/command/webview/ConnectionOverviewPage.ts
+++ dev/src/command/webview/ConnectionOverviewPage.ts
@@ -32,45 +32,131 @@ export default function getConnectionInfoPage(connectionInfo: IRemoteCodewindInf
 
         ${global.isTheia ? "" : csp}
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
         <link rel="stylesheet" href="${WebviewUtil.getStylesheetPath("connection-overview.css")}"/>
         <link rel="stylesheet" href="${WebviewUtil.getStylesheetPath("common.css")}"/>
         ${global.isTheia ?
             `<link rel="stylesheet" href="${WebviewUtil.getStylesheetPath("theia.css")}"/>` : ""}
-        <!--link rel="stylesheet" href="${WebviewUtil.getStylesheetPath("theia.css")}"/-->
+        <link rel="stylesheet" href="${WebviewUtil.getStylesheetPath("theia.css")}"/>
     </head>
     <body>
 
     <div id="top-section">
         <div class="title">
-            <img id="logo" alt="Codewind Logo" src="${WebviewUtil.getIcon(Resources.Icons.Logo)}"/>
-            <h1>${connectionInfo.label}</h1>
+            <img id="connection-logo" alt="Codewind Logo" src="${connectionInfo.url ? WebviewUtil.getIcon(Resources.Icons.ConnectionConnected) : WebviewUtil.getIcon(Resources.Icons.ConnectionDisconnected)}"/>
+            <input id="remote-connection-name" class="bx--text-input" value=${connectionInfo.label}>
         </div>
-        <div tabindex="0" id="learn-more-btn" class="btn" onclick="sendMsg('${ConnectionOverviewWVMessages.HELP}')">
+        <div tabindex="0" id="learn-more-btn"  style="color: #0f62fe;" class="btn" onclick="sendMsg('${ConnectionOverviewWVMessages.HELP}')">
             Learn More<img alt="Learn More" src="${WebviewUtil.getIcon(Resources.Icons.Help)}"/>
         </div>
     </div>
     <div id="description">
-        <p>
-            Placeholder connection description<img id="edit-description" class="btn" src="${WebviewUtil.getIcon(Resources.Icons.Edit)}"/>
-        </p>
+        <input id="description-text" class="bx--text-input-description" placeholder="Description about this remote connection that the user might use for some reason">
     </div>
     <div id="main">
-        <label for="ingress-input">Ingress URL:</label>
-        <input type="text" id="ingress-input" name="ingress-input" size="96"
-            placeholder="${connectionInfo.url ? "" : "https://codewind-workspace-mycluster.nip.io"}"
-            value="${connectionInfo.url ? connectionInfo.url : ""}"
-        />
-        <div id="submit-btn" class="btn btn-prominent" onclick="submitNewConnection()">Submit</div>
+
+            <div id="deployment-box">
+                
+                <h3>Codewind Connection
+                    ${connectionInfo.url? `<img alt="remote connection" src="${WebviewUtil.getIcon(Resources.Icons.ConnectionConnected)}"/>` : 
+                    `<img alt="remote connection" src="${WebviewUtil.getIcon(Resources.Icons.ConnectionDisconnected)}"/>`}
+                </h3>
+ 
+                <div class="input">
+                    <p>Fill in the fields about the connection that you're starting from.</p>
+
+                    <label for="input-url">URL</label>
+
+                    ${connectionInfo.url? `<div>${connectionInfo.url}</div>` :
+                    `<input type="text" id="ingress-url" class="input-url" name="ingress-url" 
+                        placeholder="${connectionInfo.url ? "" : "codewind-workspace-mycluster.nip.io"}"
+                        value="${connectionInfo.url ? connectionInfo.url : ""}"
+                    />`}
+
+                    <div style="float: left; margin-top: 10px;">
+                        <label for="input-username">Username</label>
+                        ${connectionInfo.url? `<div>${connectionInfo.username}</div>` :
+                        `<input type="text" id="ingress-username" class="input-username" name="ingress-username"
+                        value="${connectionInfo.username ? connectionInfo.username : ""}"
+                        />`}
+                    </div>
+                    <div style="overflow: hidden; margin-top: 10px;">
+                    ${connectionInfo.url ? `` :
+                        `<label for="input-password">Password</label>
+                            <input type="password" id="ingress-password" class="input-password" name="ingress-password"
+                        />`}
+                    </div>
+
+                    ${connectionInfo.url? `` : `<div type="button" id="test-btn" class="btn btn-prominent" onclick="testNewConnection()">Test</div>`}
+                </div>
+
+            </div>
+
+            <div id="deployment-box" style="margin-left: 50px;">
+                <h3>Docker Registry</h3>
+
+                <div class="input">
+                     <p>Fill in the fields about the Docker registry that you want to connect to.</p>
+
+                    <label for="input-url">URL</label>
+                        <input type="text" id="docker-url" class="input-url" name="docker-url"
+                        value="${connectionInfo.registryUrl ? connectionInfo.registryUrl : ""}"
+                    />
+
+                    <div style="float: left; margin-top: 10px;">
+                        <label for="input-username">Username</label>
+                        <input type="text" id="docker-username" class="input-username" name="docker-username"
+                        value="${connectionInfo.registryUsername ? connectionInfo.registryUsername : ""}"
+                        />
+                    </div>
+
+                    <div style="overflow: hidden; margin-top: 10px;">
+                        <label for="input-password">Password</label>
+                            <input type="password" id="docker-password" class="input-password" name="docker-password"
+                        />
+                    </div>
+
+                    <div type="button" id="test-btn" class="btn btn-prominent" onclick="testNewDockerRegistry()">Test</div>
+                </div>
+            </div>
+
+            <div>
+            ${connectionInfo.url? `<div type="button" id="delete-btn" class="btn btn-prominent" onclick="deleteConnection()">Delete Connection</div>
+                                   <div type="button" id="save-btn" class="btn btn-prominent" onclick="()">Edit</div>
+                                   <div type="button" id="cancel-btn" class="btn btn-prominent" onclick="">Disconnect</div>`
+                :
+
+                `<div type="button" id="save-btn" class="btn btn-prominent" onclick="submitNewConnection()">Save</div>
+                <div type="button" id="cancel-btn" class="btn btn-prominent" onclick="()">Cancel</div>`}
+            </div>
+
     </div>
 
     <script>
         const vscode = acquireVsCodeApi();
 
         function submitNewConnection() {
-            const ingressInput = document.querySelector("#ingress-input");
+            const ingressInput = document.querySelector("#ingress-url");
+            const ingressUsername = document.querySelector("#ingress-username");
+            const ingressPassword = document.querySelector("#ingress-password");
+            const clabel = document.querySelector("#remote-connection-name");
             // Data body is IConnectionInfoFields
-            sendMsg("${ConnectionOverviewWVMessages.SAVE_CONNECTION_INFO}", { ingressUrl: ingressInput.value });
+            sendMsg("${ConnectionOverviewWVMessages.SAVE_CONNECTION_INFO}", { ingressUrl: ingressInput.value,  username: ingressUsername.value, password: ingressPassword.value, label: clabel.value });
+        }
+
+        function testNewConnection() {
+
+        }
+
+        function deleteConnection() {
+            sendMsg("${ConnectionOverviewWVMessages.DELETE}");
+        }
+
+        function testNewDockerRegistry() {
+            const dockerRegistryURL = document.querySelector("#docker-url");
+            const dockerRegistryUsername = document.querySelector("#docker-username");
+            const dockerRegistryPassword = document.querySelector("#docker-password");
+
+            sendMsg("${ConnectionOverviewWVMessages.SAVE_REGISTRY}", { registryUrl: dockerRegistryURL.value,  registryUsername: dockerRegistryUsername.value, registryPassword: dockerRegistryPassword.value });
         }
 
         function sendMsg(type, data = undefined) {
