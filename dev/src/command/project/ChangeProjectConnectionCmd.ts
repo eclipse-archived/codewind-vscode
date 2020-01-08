843da927bdce6875d067659b2f400b279c99a325
Tim Etchells <timetchells@ibm.com>
2020-01-08 17:31:25 -0500

    Fix error handling for Move Project
    Signed-off-by: Tim Etchells <timetchells@ibm.com>

--- dev/src/command/project/ChangeProjectConnectionCmd.ts
+++ dev/src/command/project/ChangeProjectConnectionCmd.ts
@@ -19,80 +19,95 @@ import { IDetectedProjectType } from "../../codewind/connection/CLICommandRunner
 import { addProjectToConnection } from "../connection/BindProjectCmd";
 import RegistryUtils from "../../codewind/connection/RegistryUtils";
 import { removeProject } from "./RemoveProjectCmd";
+import MCUtil from "../../MCUtil";
 
 export default async function changeProjectConnectionCmd(project: Project): Promise<void> {
 
-    const selectableConnections = ConnectionManager.instance.connections.filter((conn) =>
-        // we show a given connection if the other connection is connected and does not already have this project
-        conn.isConnected && conn !== project.connection && !conn.projects.some((proj) => proj.localPath.fsPath === project.localPath.fsPath)
-    );
+    try {
+        const selectableConnections = ConnectionManager.instance.connections.filter((conn) =>
+            // we show a given connection if the other connection is connected and does not already have this project
+            conn.isConnected && conn !== project.connection && !conn.projects.some((proj) => proj.localPath.fsPath === project.localPath.fsPath)
+        );
 
-    if (selectableConnections.length === 0) {
-        vscode.window.showWarningMessage(`No suitable target for moving ${project.name} - ` +
-            `there is no connection that is connected and does not already have this project.`);
-        return;
+        if (selectableConnections.length === 0) {
+            vscode.window.showWarningMessage(`No suitable target for moving ${project.name} - ` +
+                `there is no connection that is connected and does not already have this project.`);
+            return;
+        }
+
+        const targetConnection = await vscode.window.showQuickPick(selectableConnections, {
+            canPickMany: false,
+            ignoreFocusOut: true,
+            matchOnDetail: true,
+            placeHolder: `Select the connection to move ${project.name} to.`,
+        });
+
+        if (targetConnection == null) {
+            return;
+        }
+
+        if (await RegistryUtils.doesNeedPushRegistry(project.type.internalType, targetConnection)) {
+            return;
+        }
+
+        // Ask what to do with the existing instance of the project
+        const optionRemove: vscode.QuickPickItem = {
+            label: "Remove",
+            detail: `Remove the project from ${project.connection.label}. The source code is not affected.`,
+        };
+        const optionDisable: vscode.QuickPickItem = {
+            label: "Disable",
+            detail: `Keep the project on ${project.connection.label}, but Disable it so it doesn't build there.`,
+        };
+        const optionNothing: vscode.QuickPickItem = {
+            label: "Leave it alone",
+            detail: `Leave the project running on ${project.connection.label}. It will be built on both Codewind instances.`
+        };
+
+        const options = [ optionRemove ];
+        if (project.state.isEnabled) {
+            options.push(optionDisable);
+        }
+        options.push(optionNothing);
+
+        const existingActionResponse = await vscode.window.showQuickPick(options, {
+            canPickMany: false,
+            ignoreFocusOut: true,
+            placeHolder: `Select what to do with the existing instance of ${project.name} deployed on ${project.connection.label}.`,
+        });
+
+        if (existingActionResponse == null) {
+            return;
+        }
+
+        Log.i(`Moving ${project.name} from ${project.connection.label} to ${targetConnection.label}`);
+
+        const projectType: IDetectedProjectType = {
+            language: project.language,
+            projectType: project.type.internalType,
+            // projectSubtype:
+        };
+
+        try {
+            await addProjectToConnection(targetConnection, project.name, project.localPath.fsPath, projectType);
+        }
+        catch (err) {
+            const errMsg = `Error adding ${project.name} to ${targetConnection.label}`;
+            Log.e(errMsg, err);
+            vscode.window.showErrorMessage(`${errMsg}: ${MCUtil.errToString(err)}`);
+        }
+
+        if (existingActionResponse === optionRemove) {
+            await removeProject(project, false);
+        }
+        else if (existingActionResponse === optionDisable) {
+            await toggleEnablementCmd(project);
+        }
+        // else, they selected to leave the existing project
     }
-
-    const targetConnection = await vscode.window.showQuickPick(selectableConnections, {
-        canPickMany: false,
-        ignoreFocusOut: true,
-        matchOnDetail: true,
-        placeHolder: `Select the connection to move ${project.name} to.`,
-    });
-
-    if (targetConnection == null) {
-        return;
+    catch (err) {
+        const errMsg = `Error moving ${project.name}`;
+        Log.e(errMsg, err);
+        vscode.window.showErrorMessage(`${errMsg}: ${MCUtil.errToString(err)}`);
     }
-
-    if (await RegistryUtils.doesNeedPushRegistry(project.type.internalType, targetConnection)) {
-        return;
-    }
-
-    // Ask what to do with the existing instance of the project
-    const optionRemove: vscode.QuickPickItem = {
-        label: "Remove",
-        detail: `Remove the project from ${project.connection.label}. The source code is not affected.`,
-    };
-    const optionDisable: vscode.QuickPickItem = {
-        label: "Disable",
-        detail: `Keep the project on ${project.connection.label}, but Disable it so it doesn't build there.`,
-    };
-    const optionNothing: vscode.QuickPickItem = {
-        label: "Leave it alone",
-        detail: `Leave the project running on ${project.connection.label}. It will be built on both Codewind instances.`
-    };
-
-    const options = [ optionRemove ];
-    if (project.state.isEnabled) {
-        options.push(optionDisable);
-    }
-    options.push(optionNothing);
-
-    const existingActionResponse = await vscode.window.showQuickPick(options, {
-        canPickMany: false,
-        ignoreFocusOut: true,
-        placeHolder: `Select what to do with the existing instance of ${project.name} deployed on ${project.connection.label}.`,
-    });
-
-    if (existingActionResponse == null) {
-        return;
-    }
-
-    Log.i(`Moving ${project.name} from ${project.connection.label} to ${targetConnection.label}`);
-
-    const projectType: IDetectedProjectType = {
-        language: project.language,
-        projectType: project.type.internalType,
-        // projectSubtype:
-    };
-
-    await addProjectToConnection(targetConnection, project.name, project.localPath.fsPath, projectType);
-
-    if (existingActionResponse === optionRemove) {
-        await removeProject(project, false);
-    }
-    else if (existingActionResponse === optionDisable) {
-        await toggleEnablementCmd(project);
-    }
-    // else, they want to leave the existing project
 }
